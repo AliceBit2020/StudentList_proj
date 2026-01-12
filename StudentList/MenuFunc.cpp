1b6ec0433559552cab550ac629cebf252f793434
OleksandraT <syatle13@gmail.com>
2026-01-12 20:38:40 +0200

    Add project files.

---
+++ StudentList/MenuFunc.cpp
@@ -0,0 +1,99 @@
+#include "MenuFunc.h"
+
+
+
+
+void Menu(char ptr[][30], int row)
+{
+	for (int i = 0; i < row; i++)
+	{
+		cout<<ptr[i];
+		cout << endl;
+	}
+}
+
+void MenuAddStudents(char **&oldList, int &number)
+{
+	system("cls");
+	int n;
+	cout << "Введите количество студентов для добавления в список: ";
+	cin >> n;
+	cin.get();
+	for (int i = 0; i < n; i++)
+	{
+
+		char* stud_add = new char[20];
+
+		cout << "Введите ім'я студента: ";
+		cin.getline(stud_add, 20);
+
+		AddStudents(oldList, number, stud_add);
+
+
+
+
+
+	}
+
+	system("pause");
+
+}
+
+void MenuPrint(char **list, int number)
+{
+	system("cls");
+	cout << "Список студентов: ";
+	cout << endl;
+	for (int i = 0; i < number; i++)
+		cout << list[i] << endl;
+	cout << endl;
+	system("pause");
+}
+
+void MenuSort(char **list, int number)
+{
+	system("cls");
+	/*
+		Реализация алгоритма сортировки списка студентов
+	*/
+	cout<<("Список отсортирован! ");
+	system("pause");
+}
+
+void MenuRemoveStudent(char **& oldList, int &number)
+{
+	system("cls");
+	char str[100];
+	cout<<("Введите фамилию студента для удаления: ");
+	cin.getline(str, 100);
+	/*
+		Реализация алгоритма удаления студента из списка
+	*/
+	system("pause");
+	
+}
+
+void MenuEditStudent(char **list, int number)
+{
+	system("cls");
+	char str[100];
+	cout<<("Введите фамилию студента для модификации: ");
+	cin.getline(str, 100);
+	/*
+		Реализация алгоритма модификации фамилии студента
+	*/
+	system("pause");
+
+}
+
+void MenuFindStudent(char **list, int number)
+{
+	system("cls");
+	char str[100];
+	cout<<("Введите фамилию студента или несколько первых букв: ");
+	cin.getline(str, 100);
+	/*
+		Реализация алгоритма поиска студента
+	*/
+	system("pause");
+}
---
+++ StudentList/StudListFunc.cpp
@@ -0,0 +1,38 @@
+#include "StudListFunc.h"
+
+void AddStudents(char**& list, int& count, char* student)
+{
+
+	count++;
+
+	char** temp = new char* [count];
+	for (int i = 0; i < count - 1; i++)
+		temp[i] = list[i];
+
+
+	temp[count - 1] = student;
+	delete[] list;
+
+	list = temp;
+}
+
+void RemoveStudent(char**& list, int& count, int index)
+{
+}
+
+void Print(char** list, int count)
+{
+}
+
+void Sort(char** list, int count)
+{
+}
+
+void EditStudent(char** list, int count, int index)
+{
+}
+
+int FindStudent(char** list, int count, char* student)
+{
+	return 0;
+}
